770eb454beca06d8e5cf04df6e1fa64e970b0d45
MJ-Vilarmino <mjvilarmino@gmail.com>
2023-03-27 14:57:37 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+
+
+    extend: {
+
+      colors: {
+        primary: "#23a6f0",
+        secondary: "#252b42",
+        lightGray: "#fafafa",
+        darkGray: "#737373",
+        lightBlue: "#d9edff",
+        lighterGray: "#737373",
+      },
+
+      fontFamily: {
+        monserrat: ['Montserrat', 'san-serif'],
+      },
+
+      fontSize: {
+        huge: 'clamp(3rem, 6vw, 5rem)',
+      },
+    },
+
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+        '2xl': '1496px',
+      },
+    }
+  },
+  plugins: [],
+}
